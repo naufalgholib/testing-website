2cbddc58fa443d1d5e77dc6715e9ce429eab8069
naufalgholib <naufalgholib@gmail.com>
2024-10-25 19:26:28 +0700

    benerin dockerscript

--- dockerscript.sh
+++ dockerscript.sh
@@ -1,4 +1,9 @@
-docker build -t ini-web .
-docker run -p 80:80 --name ini-website -d ini-web
+echo "Stop current docker container if there's any..." 
+docker stop ini-website && docker rm ini-website > /dev/null 2>&1
+echo "Building docker images"
+docker build -t ini-web:latest . > /dev/null 2>&1
+echo "Running container"
+docker run -p 80:80 --name ini-website -d ini-web > /dev/null 2>&1
 sleep 5
+echo "Test access"
 curl localhost:80
