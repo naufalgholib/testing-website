030dc5beabc9dfe32bac3c3b048b0bce099415b7
naufalgholib <naufalgholib@gmail.com>
2024-10-25 20:31:13 +0700

    ganti isi website

--- dockerscript.sh
+++ dockerscript.sh
@@ -1,10 +1,14 @@
-echo "Stop current docker container if there's any..." 
+echo "===Stop current docker container if there's any===" 
+echo ""
 docker stop ini-website > /dev/null 2>&1 
 docker rm ini-website > /dev/null 2>&1
-echo "Building docker images"
+echo "===Building docker images==="
+echo ""
 docker build -t ini-web:latest . > /dev/null 2>&1
-echo "Running container"
+echo "===Running container==="
+echo ""
 docker run -p 80:80 --name ini-website -d ini-web > /dev/null 2>&1
 sleep 5
-echo "Test access"
+echo "===Test access==="
+echo ""
 curl localhost:80
